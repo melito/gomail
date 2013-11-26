721183d0cd5e34ab792cb0011bf3f936bde68686
Mel Gray <melgray@gmail.com>
2013-11-25 22:49:05 -0800

    Drying things up a bit

--- gomail.go
+++ gomail.go
@@ -28,6 +28,9 @@ func main() {
 
 func startServer(port int) {
 	l, _ := net.Listen("tcp", ":"+strconv.Itoa(port))
+
+	var count int
+	count = 1
 	for {
 
 		conn, err := l.Accept()
@@ -37,70 +40,62 @@ func startServer(port int) {
 		}
 
 		client := Client{
-			Conn:    conn,
-			Text:    textproto.NewConn(conn),
-			didHelo: false,
+			Conn:     conn,
+			Text:     textproto.NewConn(conn),
+			didHelo:  false,
+			ClientId: strconv.Itoa(count),
 		}
 
 		go hanndleConnection(client)
+		count += 1
 	}
 }
 
-func (c *Client) doHelo() {
-	id, err := c.Text.Cmd("220 mx.valis.org SMTP gomail")
+func (c *Client) sendResponse(resp string) {
+	resp = resp + " " + c.ClientId + " emel"
+	id, err := c.Text.Cmd(resp)
 	if err != nil {
 		log.Println(err)
 	}
 	c.Text.StartResponse(id)
 	defer c.Text.EndResponse(id)
+}
 
+func (c *Client) doHelo() {
+	c.sendResponse("220 mx.valis.org SMTP gomail")
 	c.didHelo = true
 }
 
 func (c *Client) Close() {
-	id, _ := c.Text.Cmd("221 2.0.0 closing connection")
-	c.Text.StartResponse(id)
-	defer c.Text.EndResponse(id)
+	c.sendResponse("221 2.0.0 closing connection")
 	c.Text.Close()
 	c.Conn.Close()
 }
 
 func (c *Client) sendHello() {
-	id, _ := c.Text.Cmd("250 mx.valis.org at your service")
-	c.Text.StartResponse(id)
-	defer c.Text.EndResponse(id)
+	c.sendResponse("250 mx.valis.org at your service")
 }
 
 func (c *Client) sendCommandNotRecognized() {
-	id, _ := c.Text.Cmd("500 unrecognized command")
-	c.Text.StartResponse(id)
-	defer c.Text.EndResponse(id)
+	c.sendResponse("500 unrecognized command")
 }
 
 func (c *Client) sendCommandNotImplemented() {
-	id, _ := c.Text.Cmd("502 5.5.1 Unimplemented comand")
-	c.Text.StartResponse(id)
-	defer c.Text.EndResponse(id)
+	c.sendResponse("502 5.5.1 Unimplemented comand")
 }
 
 func (c *Client) sendWillTryMyBest() {
-	id, _ := c.Text.Cmd("252 2.1.5 Send some mail, I'll try my best")
-	c.Text.StartResponse(id)
-	defer c.Text.EndResponse(id)
+	c.sendResponse("252 2.1.5 Send some mail, I'll try my best")
 }
 
 func (c *Client) sendOk() {
-	id, _ := c.Text.Cmd("250 2.1.5 OK")
-	c.Text.StartResponse(id)
-	defer c.Text.EndResponse(id)
+	c.sendResponse("250 2.1.5 OK")
 }
 
 func (c *Client) reset() {
 	c.From = ""
 	c.Rcpt = ""
-	id, _ := c.Text.Cmd("250 2.1.5 Flushed")
-	c.Text.StartResponse(id)
-	defer c.Text.EndResponse(id)
+	c.sendResponse("250 2.1.5 Flushed")
 }
 
 func hanndleConnection(c Client) {
